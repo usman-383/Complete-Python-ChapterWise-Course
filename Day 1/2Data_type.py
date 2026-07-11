c381c000d6a72cc8757b8a54c2dd0d01216e9d48
usman-383 <usmankhan.x383@gmail.com>
2026-07-11 15:04:18 +0500

    Create Data Type

---
+++ Day 1/2Data_type.py
@@ -0,0 +1,58 @@
+#DATA TYPES 
+#1> STRING
+        #=> string is of any word or sentence.
+name1="usman"
+name2='usman'
+name3='''usman'''
+
+print(name1)
+print(name2)
+print(name3)
+
+#2>INTEGER
+         #=> integer maybe positive or negative as well as zero number but not the decimal one.
+age = 18
+print(type(age))
+
+#3 FLOAT
+      #=> float means the decimal numbers.
+price = 20.5
+print(type(price))
+
+#4 BOOLEN
+       #=> boolen means true or fals.
+age=18
+old= False
+print(type(old))
+
+#5NONE TYPE
+         #=> None type is like none of any thing.
+a= None
+print(type(a))
+
+#PRINT SUM
+a= 10
+b= 3
+sum = a+b
+print(sum)
+    #or
+a= 10
+b= 3
+print(a + b)
+
+
+#PRINT DIFFERENCE
+a= 10
+b= 3
+DIFFERENCE= a-b
+print(DIFFERENCE)
+     #or
+a= 10
+b= 3
+print(a-b)
+
+#PRINT DIVISION
+a=10
+b=3
+DIVISION=a/b
+print(DIVISION)

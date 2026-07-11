db6a7103eb3011fd341ed40d2bea0cddbfcebc93
usman-383 <usmankhan.x383@gmail.com>
2026-07-11 16:10:49 +0500

    Create Tuples

---
+++ Day 3/5Tuples.py
@@ -0,0 +1,14 @@
+#TUPLES IN PYTHON
+               # A built-in data type that lets us create immutable sequences of values.
+
+
+tup = (1, 2, 3, 4, 5)
+print(tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+print(tup[4])
+
+list = (1,)     #in tuples we use coma (,) in case of single value.
+print(type(list))

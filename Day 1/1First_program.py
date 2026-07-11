d9d618ca5fa8bfab49831837564c2a018e9bfc72
usman-383 <usmankhan.x383@gmail.com>
2026-07-11 15:03:12 +0500

    Create First Program

---
+++ Day 1/1First_program.py
@@ -0,0 +1,19 @@
+print("hello world")
+print("my name is usman")
+print("my age is 18")
+print("my name is usman" , "my age is 18")
+print(20)              #in case of number we does not use double code
+print(20 + 30)
+
+name = "usman" #stribg
+age = 18 #integer
+price = 20.5 #float
+print(name)
+print("my name is:" ,name)
+print("my age is :", age)
+
+#how to find out type 
+
+print(type(name))
+print(type(age))
+print(type(price))

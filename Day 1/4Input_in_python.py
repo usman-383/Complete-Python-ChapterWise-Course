b6f8ff5af5194e7930f68374ff610f2315cc6844
usman-383 <usmankhan.x383@gmail.com>
2026-07-11 15:53:45 +0500

    Input in python

---
+++ Day 1/4Input_in_python.py
@@ -0,0 +1,23 @@
+#INPUT IN PYTHAN
+input("enter your name :")
+name = input("enter your name :")
+print("welcome" ,name)
+
+val = input("enter some value :")
+print(type(val) , val)
+
+int("5")
+val = int(input("enter your val"))
+print(type(val), val)
+
+int("5")
+val= float(input("enter your val"))
+print(type(val) , val)
+
+name = input("enter name :")
+age = int(input("enter age :"))
+marks = float(input("enter marks :"))
+
+print("welcome" , name)
+print("age =" , age)
+print ("marks =" , marks)

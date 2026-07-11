4aa8817113e01a693f05d8067148dbc1ac6959ce
usman-383 <usmankhan.x383@gmail.com>
2026-07-11 16:08:54 +0500

    List

---
+++ Day 3/1List.py
@@ -0,0 +1,15 @@
+#LISTS IN PYTHON :
+                 #A built-in data type that store set of values. It can store elements of different types (integer , float, string, etc)
+marks = [19.5, 16.2, 17.2, 12.1,13.4]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0]) #19.5
+print(marks[1]) #16.2
+
+student = ["usman ",12.1, 19.5]
+print(student)
+
+print(student[0])
+student[0] = "luqman" #its called mutation/changes
+print(student)

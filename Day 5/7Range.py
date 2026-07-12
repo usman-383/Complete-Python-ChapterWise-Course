e0eb7a98ff5b21eb20b0afa9f8fa70697d97d370
usman-383 <usmankhan.x383@gmail.com>
2026-07-12 17:42:12 +0500

    Range

---
+++ Day 5/7Range.py
@@ -0,0 +1,20 @@
+#Range()
+      # Range function are used to return a sequence of numbers. starting from 0 by default and increase by 1 (by default), and stop before a specified number.
+
+seq = range(5)
+
+for i in seq:
+      print(i)
+
+
+for i in range(100):
+      print(i)
+
+for i in range(2,100): # start from 2 to 99
+      print(i)
+
+for i in range(2, 100, 2): # to print even num between 2 to 100
+      print(i)
+
+for i in range(1, 100, 2): # tp print odd num between 1 to 100
+      print(i)

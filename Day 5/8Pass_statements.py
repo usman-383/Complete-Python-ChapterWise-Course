e9d9438e53c7082b58656456011386fac1e6f148
usman-383 <usmankhan.x383@gmail.com>
2026-07-12 17:42:51 +0500

    Pass Statement

---
+++ Day 5/8Pass_statements.py
@@ -0,0 +1,33 @@
+# Pass statement;
+                 # pass statement is a null statement that does nothing. It is use as a placeholder for future code.
+      
+for i in range(5):
+      pass
+
+n  = 5
+sum = 0
+for i in range(1, n+1):
+      sum+=i
+
+print("total sum =", sum)
+
+
+n = 5
+sum = 0
+i = 1
+while i <= n:
+      sum += i
+      i += 1
+
+print("total sum =", sum)
+
+# write a prgram to find the factorial of n 
+
+n = 5
+fact = 1
+for i in range(1, n+1):
+      fact *= i
+      i += 1
+
+print("factorial =", fact)
+

7c8cf08bd072913bae940d49ee04d4f675360568
usman-383 <usmankhan.x383@gmail.com>
2026-07-12 17:35:52 +0500

    Sets

---
+++ Day 4/4Sets_in_Python.py
@@ -0,0 +1,12 @@
+#SET IN PYTHON:
+             # Set is the collection of the unordered items.Each elements in the set must be unique and immutable.
+
+collection = {1, 2, 3, 4, 5}
+print(collection)
+print(type(collection))
+print(len(collection))
+
+collection = {1, 2, 2, 2, "hello", "usman", "usman", 3}
+print(collection)
+print(type(collection))
+print(len(collection))

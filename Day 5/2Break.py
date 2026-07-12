05283bcbd0bd1979b2f4245d20e01f5390b22dd7
usman-383 <usmankhan.x383@gmail.com>
2026-07-12 17:38:52 +0500

    Break keyword

---
+++ Day 5/2Break.py
@@ -0,0 +1,9 @@
+#Break
+      # Break are use to terminate the loop when encounterd.
+
+i = 0
+while i <= 5:
+      print(i)
+      if(i == 3) :  # after 3 the number will not be prin.
+            break
+      i += 1

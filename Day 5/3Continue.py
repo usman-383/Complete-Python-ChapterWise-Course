74109240eb70045d65dab178cc45bdb6a8201e5e
usman-383 <usmankhan.x383@gmail.com>
2026-07-12 17:39:20 +0500

    Continue keyword

---
+++ Day 5/3Continue.py
@@ -0,0 +1,11 @@
+#Continue 
+        # Terminate exicution in the current iteration and continue exicution of the loop with the next iteration.
+
+i = 0
+
+while i <= 5:
+      if(i==3) :
+            i += 1
+            continue #3 will be skip 
+      print(i)
+      i += 1

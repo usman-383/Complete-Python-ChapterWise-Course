4894abdc573d1a110a73bf1d266296b544e7521c
usman-383 <usmankhan.x383@gmail.com>
2026-07-14 22:59:57 +0500

    Create Character.py

---
+++ Dy 7/3Character.py
@@ -0,0 +1,9 @@
+#CHARACTER AND MEANING
+
+# "r" :open for reading
+# "w" :open for writing, truncating the file first
+# "x" :create a new file and open it for writig
+# "a" :open for writing, appending it to the end of the file if its exists
+# "b" :binary mode
+# "t" :text mode
+# "+" :open a disk file for updating(reading and writing)

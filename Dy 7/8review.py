12ec5df4ace35aee54537c8a6613f711697abbe3
usman-383 <usmankhan.x383@gmail.com>
2026-07-14 23:04:57 +0500

    Review.py

---
+++ Dy 7/8review.py
@@ -0,0 +1,79 @@
+# f = open("demo.txt")
+# data = f.read()
+# print(data)
+# f.close()
+#or
+# f = open("demo.txt")
+# line2 = f.readline()
+# print(line2)
+# f.close()
+
+
+# #write
+# f = open("demo.txt","w")
+# f.write("no i am learning java")
+# f.close()
+
+
+#append
+# f = open("demo.txt","a")
+# f.write("\nmyy nike name is x5")
+# f.close()
+
+
+#read +
+# f = open("demo.txt","r+")
+# f.write("hey")
+# print(f.read())
+# f.close()
+
+
+#write +
+# f = open("demo.txt", "w+")
+# print(f.read())
+# f.write("nice")
+# f.close()
+
+
+# #append +
+# f = open("demo.txt","a+")
+# print(f.read())
+# f.write("\nnice mr x")
+# f.close()
+
+
+# with open("demo.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+
+
+# with open("demo.txt", "w") as f:
+#     f.write("nice mr x")
+
+
+# import os
+# os.remove("demo.txt")
+
+
+
+# Hi everyone
+# We are learning file i/o
+# Using java
+# I like programming in java
+
+# with open("practice.txt", "w") as f:
+#     f.write("Hi everyone.\nWe are learning file i/o\n")
+#     f.write("Using java.\nI like programming in java")
+
+
+
+# with open("practice.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+# new_data = data.replace("java", "python")
+# print(new_data)
+
+# with open("practice.txt", "w") as f:
+#     f.write(new_data)

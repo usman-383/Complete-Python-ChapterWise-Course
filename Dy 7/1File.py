99fbb0bbec29c3ac5bc1135b44ffa40b7dcec0d4
usman-383 <usmankhan.x383@gmail.com>
2026-07-14 22:49:39 +0500

    Create 1File.py

---
+++ Dy 7/1File.py
@@ -0,0 +1,7 @@
+#FILE I/O IN PYTHON:
+                 #Python can be use to perform operations on a file.(read and write data)
+
+#TYPES OF FILE:
+
+#1) Text fil :->  .txt, .docx, .log 
+#2) Binary Files :-> .mp4, .mov, .png, jpeg

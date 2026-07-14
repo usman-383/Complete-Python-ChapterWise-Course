665962c6f45d0869b3ff96d2d515004be4c2c651
usman-383 <usmankhan.x383@gmail.com>
2026-07-14 23:03:34 +0500

    Deleting File

---
+++ Dy 7/6Deleting_a_file.py
@@ -0,0 +1,7 @@
+#DELETING A FILE:
+ #Using the os module
+ #Module (like a code library) is a file written by another programmer that generally has a function we can use
+
+#pip install tensorflow
+
+#os.remove("demo.txt")

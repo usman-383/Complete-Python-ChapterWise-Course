1778a46168457fb61dae21802fc270053cd62c85
usman-383 <usmankhan.x383@gmail.com>
2026-07-14 23:08:15 +0500

    Del keyword

---
+++ Day 9/1Del_keyword.py
@@ -0,0 +1,11 @@
+#DEL KEYWORD:
+           #Use to delete object properties or object itself.
+
+class student:
+    def __init__(self, name):
+        self.name = name 
+
+s1 = student("usman")
+print(s1)
+print(s1.name)
+del s1

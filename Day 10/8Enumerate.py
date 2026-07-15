4aaa4e1eb767477859691312e7587d6c458dc111
usman-383 <usmankhan.x383@gmail.com>
2026-07-15 08:16:46 +0500

    Enumarate

---
+++ Day 10/8Enumerate.py
@@ -0,0 +1,12 @@
+#Enumerate
+
+l = [2, 3, 54,65]
+
+idx = 0
+for items in l:
+    print(f"The item number at index{idx} is {items}")
+    idx +=1
+
+
+for index,items in enumerate:
+    print(f"The items number at index {index} is {items}")

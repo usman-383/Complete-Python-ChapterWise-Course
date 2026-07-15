b4128e0466ab9edaa68c86641476b188ef21c6de
usman-383 <usmankhan.x383@gmail.com>
2026-07-15 08:17:50 +0500

    Comprehension

---
+++ Day 10/10Comprehension.py
@@ -0,0 +1,14 @@
+
+#comprehesion
+
+mylist = [1, 2, 4, 5, 6, 8]
+squaredlist = []
+
+for items in mylist:
+    squaredlist.appent(items * items)
+print(squaredlist)
+
+
+mylist = [1,2, 4,6]
+squarelist = [i*i for i in mylist]
+print(squarelist)

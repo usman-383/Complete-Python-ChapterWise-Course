6203a7f5c9c33398956d1046778bcd091f282210
usman-383 <usmankhan.x383@gmail.com>
2026-07-16 09:21:42 +0500

    Images added

---
+++ Projects/Project 1/Static/script.js
@@ -0,0 +1,9 @@
+function playSound(name) {
+    const sound = document.getElementById(name);
+    if (sound) {
+        sound.currentTime = 0;
+        sound.play().catch(err => {
+            console.log("Sound blocked:", err);
+        });
+    }
+}

6cdb5198f7d5c735eaf4e63ca68d65cc599b9ce7
usman-383 <usmankhan.x383@gmail.com>
2026-07-24 13:53:01 +0500

    Create speech recognition

---
+++ Projects/Mega Project 1/musiclibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "s1" : "https://youtu.be/U47Tr9BB_wE?si=qG0hWVvvcYsDvueb",
+    "jol" : "https://youtu.be/-2RAq5o5pwc?si=EZlLE9auBYPuXNeG",
+    "funny" : "https://youtu.be/B7FpOiOLQzo?si=WgUH4IOiYo8wUbce",
+    "techno" : "https://youtu.be/Be_IPqZFdhs",
+    "bmw" : "https://youtu.be/ueGoScRwCp8?si=WzVpr83LOFIdBNUe"
+
+}
